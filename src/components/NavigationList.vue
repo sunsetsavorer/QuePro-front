67c441549966e39794d8edc0e4007d4eca915fb3
s11en1 <sunset.savorer@gmail.com>
2024-12-07 15:24:41 +0300

    add navigation list component

---
+++ src/components/NavigationList.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'NavigationList'
+}
+</script>
+
+<template>
+    <ul class="navigation-list">
+        <li><a href="">Новости</a></li>
+        <li><a href="">Турниры</a></li>
+        <li><a href="">Контакты</a></li>
+        <li><a href="">Личный кабинет</a></li>
+    </ul>
+</template>
+
+<style scoped>
+a {
+    color: var(--color-text-primary);
+
+    font-family: var(--montserrat);
+    font-weight: 500;
+    font-size: 22px;
+
+    text-wrap: nowrap;
+}
+</style>
