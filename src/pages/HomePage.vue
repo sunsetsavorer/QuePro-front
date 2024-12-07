074e7e71f9e55f7aa6a19b14f5d03151d8ef39bb
s11en1 <sunset.savorer@gmail.com>
2024-12-07 19:33:02 +0300

    create home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,23 @@
+<script>
+import HeroBlock from '@/components/HeroBlock.vue';
+
+export default {
+    name: 'HomePage',
+    components: {
+        HeroBlock,
+    }
+}
+</script>
+
+<template>
+    <div class="home-page">
+        <HeroBlock
+            title="QuePro"
+            subtitle=
+            "Организатор Киберспортивных Событий"
+            videoSource="/src/assets/videos/hero-block-bg.mp4"
+            ctaText="СМОТРЕТЬ"
+            ctaLink="https://www.youtube.com/@LofiGirl"
+        />
+    </div>
+</template>
