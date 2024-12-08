5cdc558e1c07eaaa131cb2037da60b901a051635
s11en1 <sunset.savorer@gmail.com>
2024-12-08 15:31:49 +0300

    add about section component

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,109 @@
+<script>
+export default {
+    name: 'AboutSection',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        mobilePhotoSource: {
+            type: String,
+            required: true,
+        },
+        pcPhotoSource: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            isMobile: false,
+        }
+    },
+    methods: {
+        handleWindowResize() {
+            this.isMobile = window.innerWidth <= 768;
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.handleWindowResize);
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.handleWindowResize);
+    },
+}
+</script>
+
+<template>
+    <section class="about-section g-wrapper">
+        <div class="about-section__texts-wrapper">
+            <h2 class="about-section__title g-title">{{ title }}</h2>
+            <p class="about-section__text">{{ text }}</p>
+        </div>
+        <img
+            class="about-section__image"
+            alt="QuePro"
+            :src="isMobile
+                ? mobilePhotoSource
+                : pcPhotoSource"
+        >
+    </section>
+</template>
+
+<style scoped>
+.about-section {
+    padding-top: 10vh;
+    padding-bottom: 10vh;
+
+    display: flex;
+    justify-content: space-around;
+    gap: 20px;
+}
+
+.about-section__texts-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+}
+
+.about-section__image {
+    width: 340px;
+    height: 550px;
+}
+
+.about-section__text {
+    font-family: var(--roboto);
+    color: var(--color-text-primary);
+    font-size: 24px;
+
+    max-width: 500px;
+}
+
+@media (max-width: 940px) {
+    .about-section__text {
+        font-size: 20px;
+    }
+}
+
+@media (max-width: 768px) {
+    .about-section {
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .about-section__text {
+        font-size: 18px;
+        max-width: 100%;
+    }
+
+    .about-section__image {
+        width: 100%;
+        height: auto
+    }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,12 @@
 <script>
 import HeroBlock from '@/components/HeroBlock.vue';
+import AboutSection from '@/components/AboutSection.vue';
 
 export default {
     name: 'HomePage',
     components: {
         HeroBlock,
+        AboutSection,
     }
 }
 </script>
@@ -18,6 +20,15 @@ export default {
             videoSource="/src/assets/videos/hero-block-bg.mp4"
             ctaText="СМОТРЕТЬ"
             ctaLink="https://www.youtube.com/@LofiGirl"
+        />
+        <AboutSection
+            title="О нас"
+            mobilePhotoSource="/src/assets/images/about_us_section/mobile.png"
+            pcPhotoSource="/src/assets/images/about_us_section/pc.png"
+            text="Мы — QuePro, платформа для киберспортсменов и любителей игр.
+                Организуем и освещаем самые захватывающие турниры, чтобы каждый участник и
+                фанат мог погрузиться в мир профессионального гейминга. Присоединяйтесь к нам,
+                чтобы следить за новостями, участвовать в событиях и поддерживать своих фаворитов!"
         />
     </div>
 </template>
