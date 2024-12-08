cf6d2503b42a8f1ddc84ca60a2c08633c8ca2e95
s11en1 <sunset.savorer@gmail.com>
2024-12-08 17:34:04 +0300

    add partners component

---
+++ src/components/PartnersSection.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    name: 'PartnersSection',
+    props: {
+        partners: {
+            type: Array,
+            default: [],
+        },
+    }
+}
+</script>
+
+<template>
+    <section class="partners-section g-wrapper">
+        <h2 class="g-title g-title--dark partners-section__title">Наши партнеры</h2>
+        <div class="partners-section__partners-list">
+            <img
+                class="partners-section__partner-icon"
+                v-for="(partnerSrc, index) in partners" :key="index"
+                :src="partnerSrc"
+                alt="Иконка партнера"
+            >
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .partners-section {
+        background-color: var(--color-accent);
+
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+
+    .partners-section__partners-list {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 30px;
+    }
+
+    .partners-section__partners-list,
+    .partners-section__partner-icon {
+        transition: .3s ease
+    }
+
+    .partners-section__title {
+        margin-bottom: 30px;
+    }
+
+    @media (max-width: 768px) {
+        .partners-section__partners-list {
+            gap: 15px;
+        }
+
+        .partners-section__partner-icon {
+            height: 50px;
+        }
+    }
+
+    @media (max-width: 375px) {
+        .partners-section__partner-icon {
+            height: 30px;
+        }
+    }
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,14 @@
 <script>
 import HeroBlock from '@/components/HeroBlock.vue';
 import AboutSection from '@/components/AboutSection.vue';
+import PartnersSection from '@/components/PartnersSection.vue';
 
 export default {
     name: 'HomePage',
     components: {
         HeroBlock,
         AboutSection,
+        PartnersSection,
     }
 }
 </script>
@@ -29,6 +31,21 @@ export default {
                 Организуем и освещаем самые захватывающие турниры, чтобы каждый участник и
                 фанат мог погрузиться в мир профессионального гейминга. Присоединяйтесь к нам,
                 чтобы следить за новостями, участвовать в событиях и поддерживать своих фаворитов!"
+        />
+        <PartnersSection
+            :partners="[
+                '/src/assets/images/partners/adobe.svg',
+                '/src/assets/images/partners/github.svg',
+                '/src/assets/images/partners/hubspot.svg',
+                '/src/assets/images/partners/ibm.svg',
+                '/src/assets/images/partners/notion.svg',
+                '/src/assets/images/partners/gitlab.svg',
+                '/src/assets/images/partners/google.svg',
+                '/src/assets/images/partners/tinder.svg',
+                '/src/assets/images/partners/zoom.svg',
+                '/src/assets/images/partners/spotify.svg',
+                '/src/assets/images/partners/microsoft.svg',
+            ]"
         />
     </div>
 </template>
