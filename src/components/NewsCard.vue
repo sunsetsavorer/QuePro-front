d710fb08d56f8db4a1d3412d868d1dc29cba2efb
s11en1 <sunset.savorer@gmail.com>
2024-12-09 01:17:14 +0300

    add news card component

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+    name: 'NewsCard',
+    props: {
+        data: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="news-card">
+        <div class="news-card__texts">
+            <p class="news-card__title">{{ data.title }}</p>
+            <p class="news-card__date">{{ data.publication_date }}</p>
+        </div>
+        <img
+            class="news-card__image"
+            :src="data.picture_path"
+            alt="Картинка Новости"
+        >
+    </div>
+</template>
+
+<style scoped>
+    .news-card {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        height: 350px;
+        width: 300px;
+
+        background-color: #2E2E2E;
+    }
+
+    .news-card__texts {
+        padding: 20px;
+    }
+
+    .news-card__title {
+        font-family: var(--roboto);
+        color: var(--color-text-primary);
+
+        margin-bottom: 20px;
+    }
+
+    .news-card__date {
+        font-family: var(--roboto);
+        color: #ACACAC;
+    }
+</style>
