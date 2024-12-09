99422add660f941506e8f21c0f0e0cdb1141e185
s11en1 <sunset.savorer@gmail.com>
2024-12-09 14:08:03 +0300

    add news detail route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "@/pages/HomePage.vue";
+import NewsDetailPage from "@/pages/NewsDetailPage.vue";
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: HomePage,
+    },
+    {
+        path: '/news/:slug',
+        name: 'NewsDetail',
+        component: NewsDetailPage,
+        props: true,
     }
 ];
 
