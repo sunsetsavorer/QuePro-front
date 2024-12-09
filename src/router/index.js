ae2ec248a5b67c5593e2a71c468ac4cff51fbf09
s11en1 <sunset.savorer@gmail.com>
2024-12-09 19:20:43 +0300

    add new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// Pages
 import HomePage from "@/pages/HomePage.vue";
 import NewsDetailPage from "@/pages/NewsDetailPage.vue";
+import PaginationPage from "@/pages/PaginationPage.vue";
+
+// Components
+import NewsCard from "@/components/NewsCard.vue";
+import TournamentCard from "@/components/TournamentCard.vue";
+
+// Functions
+import { fetchNews } from "@/services/newsService";
+import { fetchTournamnents } from "@/services/tournamentService";
 
 const routes = [
     {
@@ -14,7 +24,28 @@ const routes = [
         name: 'NewsDetail',
         component: NewsDetailPage,
         props: true,
-    }
+    },
+    {
+        path: '/news',
+        name: 'News',
+        component: PaginationPage,
+        props: {
+            title: 'Новости',
+            cardComponent: NewsCard,
+            fetchFunction: fetchNews,
+        },
+    },
+    {
+        path: '/tournaments',
+        name: 'Tournaments',
+        component: PaginationPage,
+        props: {
+            title: 'Турниры',
+            cardComponent: TournamentCard,
+            fetchFunction: fetchTournamnents,
+        },
+    },
+
 ];
 
 const router = createRouter({
